3432aebbf51df95b048bf0b1b42307764bbed13e
Alex Chen <laplacedoge@gmail.com>
2022-11-05 01:28:56 +0800

    [utf8-codec] add decoding test for partial invalid encoded data

--- utf8-codec/test.c
+++ utf8-codec/test.c
@@ -5,6 +5,7 @@
 #include "utf8_codec.h"
 
 static const char encoded_data_only_ascii[] = " _123abcABC@#$\t\r\n\a";
+
 static const uint16_t decoded_data_only_ascii[] =
 {
     0x0020, 0x005F, 0x0031, 0x0032, 0x0033, 0x0061, 0x0062, 0x0063,
@@ -12,7 +13,9 @@ static const uint16_t decoded_data_only_ascii[] =
     0x000A, 0x0007
 };
 
-static const char encoded_data_only_greek_alphabet[] = "ΑαΒβΓγΔδΕεΖζΗηΘθΙιΚκΛλΜμΝνΞξΟοΠπΡρΣσςΤτΥυΦφΧχΨψΩω";
+static const char encoded_data_only_greek_alphabet[] =
+    "ΑαΒβΓγΔδΕεΖζΗηΘθΙιΚκΛλΜμΝνΞξΟοΠπΡρΣσςΤτΥυΦφΧχΨψΩω";
+
 static const uint16_t decoded_data_only_greek_alphabet[] =
 {
     0x0391, 0x03B1, 0x0392, 0x03B2, 0x0393, 0x03B3, 0x0394, 0x03B4,
@@ -25,14 +28,17 @@ static const uint16_t decoded_data_only_greek_alphabet[] =
 };
 
 static const char encoded_data_only_chinese[] = "工欲善其事，必先利其器。";
+
 static const uint16_t decoded_data_only_chinese[] =
 {
     0x5DE5, 0x6B32, 0x5584, 0x5176, 0x4E8B, 0xFF0C, 0x5FC5, 0x5148,
     0x5229, 0x5176, 0x5668, 0x3002
 };
 
-static const char encoded_data_triple_mixed[] = "\t<p>是的，first let's 设置 β to 1.9, 然后静观其变！</p>";
-static const uint16_t decoded_data_triple_mixed[] =
+static const char encoded_data_triple_mix[] =
+    "\t<p>是的，first let's 设置 β to 1.9, 然后静观其变！</p>";
+
+static const uint16_t decoded_data_triple_mix[] =
 {
     0x0009, 0x003C, 0x0070, 0x003E, 0x662F, 0x7684, 0xFF0C, 0x0066,
     0x0069, 0x0072, 0x0073, 0x0074, 0x0020, 0x006C, 0x0065, 0x0074,
@@ -50,25 +56,73 @@ void test_decode_right_encoded_data(void)
 
     printf("\n>>> TEST: decode right encoded data\n");
 
-    ret = utf8_decode(code_points, encoded_data_only_ascii, sizeof(encoded_data_only_ascii) - 1, &decoded_num, NULL, 0);
+    ret = utf8_decode(code_points, encoded_data_only_ascii,
+                      sizeof(encoded_data_only_ascii) - 1,
+                      &decoded_num, NULL, 0);
     assert(ret == UTF8DEC_OK);
     assert(decoded_num == 18);
     assert(memcmp(code_points, decoded_data_only_ascii, 18) == 0);
 
-    ret = utf8_decode(code_points, encoded_data_only_greek_alphabet, sizeof(encoded_data_only_greek_alphabet) - 1, &decoded_num, NULL, 0);
+    ret = utf8_decode(code_points, encoded_data_only_greek_alphabet,
+                      sizeof(encoded_data_only_greek_alphabet) - 1,
+                      &decoded_num, NULL, 0);
     assert(ret == UTF8DEC_OK);
     assert(decoded_num == 49);
     assert(memcmp(code_points, decoded_data_only_greek_alphabet, 49) == 0);
 
-    ret = utf8_decode(code_points, encoded_data_only_chinese, sizeof(encoded_data_only_chinese) - 1, &decoded_num, NULL, 0);
+    ret = utf8_decode(code_points, encoded_data_only_chinese,
+                      sizeof(encoded_data_only_chinese) - 1,
+                      &decoded_num, NULL, 0);
     assert(ret == UTF8DEC_OK);
     assert(decoded_num == 12);
     assert(memcmp(code_points, decoded_data_only_chinese, 12) == 0);
 
-    ret = utf8_decode(code_points, encoded_data_triple_mixed, sizeof(encoded_data_triple_mixed) - 1, &decoded_num, NULL, 0);
+    ret = utf8_decode(code_points, encoded_data_triple_mix,
+                      sizeof(encoded_data_triple_mix) - 1,
+                      &decoded_num, NULL, 0);
     assert(ret == UTF8DEC_OK);
     assert(decoded_num == 43);
-    assert(memcmp(code_points, decoded_data_triple_mixed, 43) == 0);
+    assert(memcmp(code_points, decoded_data_triple_mix, 43) == 0);
+
+    printf("<<< PASS\n");
+}
+
+static const char encoded_data_partial_invalid_triple_mix[] =
+    "\t<p>是\x80的，first let\xFF'\xC3s 设置 β to 1.9, 然后\x80\xA7静观其变！</p>";
+
+static const uint16_t decoded_data_partial_invalid_triple_mix[] =
+{
+    0x0009, 0x003C, 0x0070, 0x003E, 0x662F, 0x7684, 0xFF0C, 0x0066,
+    0x0069, 0x0072, 0x0073, 0x0074, 0x0020, 0x006C, 0x0065, 0x0074,
+    0x0027, 0x0073, 0x0020, 0x8BBE, 0x7F6E, 0x0020, 0x03B2, 0x0020,
+    0x0074, 0x006F, 0x0020, 0x0031, 0x002E, 0x0039, 0x002C, 0x0020,
+    0x7136, 0x540E, 0x9759, 0x89C2, 0x5176, 0x53D8, 0xFF01, 0x003C,
+    0x002F, 0x0070, 0x003E
+};
+
+void test_decode_partial_invalid_encoded_data(void)
+{
+    uint16_t code_points[1024];
+    size_t decoded_num;
+    size_t last_pos;
+    int ret;
+
+    printf("\n>>> TEST: decode partial invalid encoded data\n");
+
+    ret = utf8_decode(code_points,
+                      encoded_data_partial_invalid_triple_mix,
+                      sizeof(encoded_data_partial_invalid_triple_mix) - 1,
+                      &decoded_num, &last_pos, 0);
+    assert(ret == UTF8DEC_ERR_INVALID_DATA);
+    assert(last_pos == 7);
+
+    ret = utf8_decode(code_points,
+                      encoded_data_partial_invalid_triple_mix,
+                      sizeof(encoded_data_partial_invalid_triple_mix) - 1,
+                      &decoded_num, NULL, UTF8DEC_IGNORE_INVALID_DATA);
+    assert(ret == UTF8DEC_OK);
+    assert(decoded_num == 43);
+    assert(memcmp(code_points, decoded_data_partial_invalid_triple_mix, 43) == 0);
 
     printf("<<< PASS\n");
 }
@@ -76,6 +130,8 @@ void test_decode_right_encoded_data(void)
 int main(int argc, char **argv)
 {
     test_decode_right_encoded_data();
+
+    test_decode_partial_invalid_encoded_data();
 
     return 0;
 }
